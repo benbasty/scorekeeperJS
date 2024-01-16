e852caa16f32da18486dc611d99c28d4a5f94146
benbasty <benbasty@yahoo.fr>
2024-01-17 07:18:34 +0800

    ScoreKeeper

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
 
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 
+const winningScoreSelect = document.querySelector('#playTo');
+
+
 let p1Score = 0;
 let p2Score = 0;
-let winingScore = 5;
+let winingScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function() {
@@ -14,7 +18,12 @@ p1Button.addEventListener('click', function() {
         p1Score +=1;
         if(p1Score === winingScore) {
             isGameOver = true;
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
+
         p1Display.textContent = p1Score;
     }
 })
@@ -24,7 +33,30 @@ p2Button.addEventListener('click', function() {
         p2Score +=1;
         if(p2Score === winingScore) {
             isGameOver = true;
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
+})
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('has-text-success','has-text-danger');
+    p2Display.classList.remove('has-text-success','has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
+}
+
+resetButton.addEventListener('click', reset);
+
+winningScoreSelect.addEventListener('change', function() {
+    winingScore = parseInt(this.value);
+    reset();
 })
